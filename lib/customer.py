5355f70c4c4b81086697d3c5d9154ad1f2ac74fd
Frankincense <frankincensewelsey@gmail.com>
2025-05-17 11:25:39 +0300

    Customer test and class finished with getter and setter method.

--- lib/customer.py
+++ lib/customer.py
@@ -1,2 +1,15 @@
 class Customer:
-    pass
+    def __init__(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise Exception(f"{name} must be a string of length 1 - 15 ")
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 0 < len(name) < 16:
+            self._name = name
+        else:
+            raise Exception(f"{name} must be a string of length 1 - 15 ")
--- lib/tests/customer_test.py
+++ lib/tests/customer_test.py
@@ -1,5 +1,21 @@
 from customer import Customer 
+import pytest
 
 class TestCustomer:
     def test_instatiates_with_str(self):
+        '''instantiates with a single argument, a string.'''
         assert(Customer("Frank"))
+
+    def test_name_is_is_valid(self):
+        '''Name is a str between 1 and 15 characters in length.'''
+        with pytest.raises(Exception):
+            Customer(0)
+            Customer("")
+
+    def test_Customer_name_change(self):
+        '''Customer class type checks name to be string.'''
+        frank = Customer("Frank")
+        with pytest.raises(Exception):
+            frank.name = 0
+            len(frank.name) == 0
+            frank.name = " "
