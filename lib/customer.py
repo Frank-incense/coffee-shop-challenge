c99ae155669c64ca7b1eca92ae2d9c91c2ab1055
Frankincense <frankincensewelsey@gmail.com>
2025-05-18 19:07:02 +0300

    Customer class now has orders and coffees methods

--- lib/customer.py
+++ lib/customer.py
@@ -6,12 +6,23 @@ class Customer:
             type(self).all.append(self)
         else:
             raise Exception(f"{name} must be a string of length 1 - 15 ")
+        
     @property
     def name(self):
         return self._name
+    
     @name.setter
     def name(self, name):
         if isinstance(name, str) and 0 < len(name) < 16:
             self._name = name
         else:
             raise Exception(f"{name} must be a string of length 1 - 15 ")
+        
+    def orders(self):
+        from order import Order
+        return [order for order in Order.all if self == order.customer]
+    
+
+    def coffees(self):
+        from order import Order
+        return [order.coffee for order in Order.all if self == order.customer]
--- lib/tests/customer_test.py
+++ lib/tests/customer_test.py
@@ -1,4 +1,6 @@
-from customer import Customer 
+from customer import Customer
+from coffee import Coffee
+from order import Order
 import pytest
 
 class TestCustomer:
@@ -10,12 +12,36 @@ class TestCustomer:
         '''Name is a str between 1 and 15 characters in length.'''
         with pytest.raises(Exception):
             Customer(0)
+        with pytest.raises(Exception):
             Customer("")
 
     def test_Customer_name_change(self):
         '''Customer class type checks name to be string.'''
         frank = Customer("Frank")
+    
         with pytest.raises(Exception):
             frank.name = 0
             len(frank.name) == 0
             frank.name = " "
+
+    def test_customer_has_orders(self):
+        '''Test Customer class has method orders that retruns list of orders'''
+        frank = Customer("Frank")
+        espresso = Coffee("Espresso")
+        price = 10.0
+
+        order = Order(frank, espresso, price)
+
+        assert frank.orders() == [order]
+
+    def test_customer_has_coffees(self):
+        '''Test Customer class has method orders that retruns list of orders'''
+        frank = Customer("Frank")
+        espresso = Coffee("Espresso")
+        price = 10.0
+
+        Order(frank, espresso, price)
+
+        assert espresso in frank.coffees()
+
+    
